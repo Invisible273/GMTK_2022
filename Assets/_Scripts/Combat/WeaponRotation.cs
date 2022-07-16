78bca075ef3a0670b12557c416b3e15e5ef9f2a3
Adrian Kovatana <adrian.kovatana@gmail.com>
2022-07-16 12:43:08 -0500

    Add missing files, hopefully all of them

---
+++ Assets/_Scripts/Combat/WeaponRotation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponRotation : MonoBehaviour
+{
+    private PlayerController playerController;
+
+    private void Awake() {
+        playerController = transform.parent.GetComponent<PlayerController>();
+
+        playerController.onMousePositionUpdate += OnMousePositionUpdate;
+    }
+
+    private void OnMousePositionUpdate(Vector3 mousePos) {
+        Vector3 difference = mousePos - transform.position;
+        difference.Normalize();
+        float rotationAngle = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationAngle);
+    }
+}
