565831426d5da70758c3a30851622b5225db179f
Invisible273 <invisible273@gmail.com>
2022-07-16 15:07:29 +0300

    Added new Input System and rolling (no number change)
    Continious collision on rolling into walls causes visual jitter, fix if have time.

--- Assets/_Scripts/Characters/Player.cs
+++ Assets/_Scripts/Characters/Player.cs
@@ -6,9 +6,25 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
+    private const float ROLL_THRESHOLD = 5f;
+
+    [SerializeField] private LayerMask collisionLayerMask;
     [SerializeField] private float movementSpeed;
+    [SerializeField] private float maxRollSpeed;
+    [SerializeField] private float rollSpeedDecay;
+
     private PlayerController playerController;
     private Rigidbody2D rb;
+    private Vector2 walkVector;
+    private float currentRollSpeed;
+    private Vector3 currentRollDir;
+
+    private State state;
+    private enum State
+    {
+        Normal,
+        Rolling,
+    }
 
     private void Start()
     {
@@ -16,10 +32,47 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         playerController.onMovementInput += OnMovementInputRecieved;
+        playerController.onRollInput += OnRollInputRecieved;
+
+        walkVector = Vector2.zero;
+        state = State.Normal;
     }
 
     private void OnMovementInputRecieved(Vector2 movementDir)
     {
-        rb.velocity = movementDir * movementSpeed;
+        walkVector = movementDir * movementSpeed;
+    }
+
+    private void OnRollInputRecieved(Vector2 movementDir)
+    {
+        state = State.Rolling;
+        currentRollSpeed = maxRollSpeed;
+        currentRollDir = new Vector3(movementDir.x, movementDir.y, 0);
+    }
+
+    private void FixedUpdate() 
+    {
+        switch (state) 
+        {
+            case State.Normal:
+                HandleMovement();
+                break;
+            case State.Rolling:
+                HandleRolling();
+                break;
+        }
+    }
+
+    private void HandleMovement()
+    {
+        rb.velocity = walkVector;
+    }
+
+    private void HandleRolling()
+    {
+        transform.position += currentRollDir * currentRollSpeed * Time.deltaTime;
+        currentRollSpeed -= currentRollSpeed * rollSpeedDecay * Time.deltaTime;
+        if (currentRollSpeed < ROLL_THRESHOLD)
+            state = State.Normal;
     }
 }
--- Assets/_Scripts/Control/PlayerController.cs
+++ Assets/_Scripts/Control/PlayerController.cs
@@ -2,26 +2,62 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private PlayerInputActions playerInput;
     private Vector2 previousMovementVector;
 
+    private void Awake()
+    {
+        playerInput = new PlayerInputActions();
+        playerInput.Player.Roll.performed += Roll;
+    }
+    
     private void Start()
     {
         previousMovementVector = Vector2.zero;
     }
 
+    private void OnEnable()
+    {
+        playerInput.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Player.Disable();
+    }
+
     private void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 movementVector = new Vector2(horizontal, vertical);
-        
+        Vector2 movementVector = playerInput.Player.Move.ReadValue<Vector2>();
+
         if (movementVector != previousMovementVector)
         {
             MovementInputRecieved(movementVector);
             previousMovementVector = movementVector;
+        }
+    }
+
+    public void Roll(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            Vector2 movementVector = playerInput.Player.Move.ReadValue<Vector2>();
+            if (movementVector == Vector2.zero)
+                return;
+            if (Mathf.Abs(movementVector.x) > Mathf.Abs(movementVector.y))
+            {
+                Vector2 rollDir = Mathf.Sign(movementVector.x) * Vector2.right;
+                RollInputRecieved(rollDir);
+            }
+            else
+            {
+                Vector2 rollDir = Mathf.Sign(movementVector.y) * Vector2.up;
+                RollInputRecieved(rollDir);
+            }
         }
     }
 
@@ -30,5 +66,11 @@ public class PlayerController : MonoBehaviour
     public void MovementInputRecieved(Vector2 movDir)
     {
         onMovementInput?.Invoke(movDir);
+    }
+
+    public event MovementInput onRollInput;
+    public void RollInputRecieved(Vector2 movDir)
+    {
+        onRollInput?.Invoke(movDir);
     }
 }
